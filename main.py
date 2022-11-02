2dc402176e869849f67c4960fab153cb311114f3
Enmanuel Proano <rbsrman123@gmail.com>
2022-11-02 19:45:43 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Python Project 1 Calculator
+
+print("Welcome to the calculator")
+print("What would you want to do?")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
