8352fcf84736cbdcd8484106d9ca5fef31d91ded
Enmanuel Proano <rbsrman123@gmail.com>
2022-11-02 20:10:56 -0400

    finished edit

--- main.py
+++ main.py
@@ -8,27 +8,31 @@ programRun = True
 
 while programRun: # make program run continuously
     os.system('cls')
-    print("Welcome to the calculator (2 numbers)")
+    print("Welcome to the calculator (2 numbers) \n")
     print("What would you want to do?")
     print("1. Add")
     print("2. Subtract")
     print("3. Multiply")
     print("4. Divide")
-    print("5. Quit")
+    print("5. Quit \n")
 
     userInput = int(input("Enter input: "))
 
     if userInput == 1: # call function from module
         mathFunc.addFunction()
+        os.system('pause')
         
     if userInput == 2:
         mathFunc.subtractFunction()
+        os.system('pause')
         
     if userInput == 3:
         mathFunc.multiplyFunction()
+        os.system('pause')
         
     if userInput == 4:
         mathFunc.divideFunction()
+        os.system('pause')
         
     if userInput == 5:
         print("Goodbye")
--- mathFunc.py
+++ mathFunc.py
@@ -5,12 +5,20 @@ def addFunction():
     print("What is the second number?: ")
     secondNum = int(input("Second number: "))
     
+    total = firstNum + secondNum
+    print(f"Total: {total}")
+    
+    
 def subtractFunction(): 
     print("What is the first number?: ")
     firstNum = int(input("First number: "))
         
     print("What is the second number?: ")
     secondNum = int(input("Second number: "))
+    
+    total = firstNum - secondNum
+    print(f"Total: {total}")
+    
     
 def multiplyFunction(): 
     print("What is the first number?: ")
@@ -19,10 +27,18 @@ def multiplyFunction():
     print("What is the second number?: ")
     secondNum = int(input("Second number: "))
     
+    total = firstNum * secondNum
+    print(f"Total: {total}")
+    
+    
 def divideFunction(): 
     print("What is the first number?: ")
     firstNum = int(input("First number: "))
         
     print("What is the second number?: ")
     secondNum = int(input("Second number: "))
+    
+    total = firstNum / secondNum
+    print(f"Total: {total}")
+    
     
